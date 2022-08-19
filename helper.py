554e51102aa3c08a1f9e6dd5d7922f99243161fa
machinelearnear <machinelearnear@gmail.com>
2022-08-19 11:41:06 +0000

    add get_video_it

--- helper.py
+++ helper.py
@@ -118,4 +118,39 @@ def vid2frames(url, sampling_interval=1, **kwargs):
     else:
         skip_frames, path_frames = None, None
         
-    return(skip_frames, path_frames, video)
+    return(skip_frames, path_frames)
+
+def get_yt_video_id(url):
+    """Returns Video_ID extracting from the given url of Youtube
+    
+    Source: https://gist.github.com/kmonsoor/2a1afba4ee127cce50a0
+    
+    Examples of URLs:
+      Valid:
+        'http://youtu.be/_lOT2p_FCvA',
+        'www.youtube.com/watch?v=_lOT2p_FCvA&feature=feedu',
+        'http://www.youtube.com/embed/_lOT2p_FCvA',
+        'http://www.youtube.com/v/_lOT2p_FCvA?version=3&amp;hl=en_US',
+        'https://www.youtube.com/watch?v=rTHlyTphWP0&index=6&list=PLjeDyYvG6-40qawYNR4juzvSOg-ezZ2a6',
+        'youtube.com/watch?v=_lOT2p_FCvA',
+      
+      Invalid:
+        'youtu.be/watch?v=_lOT2p_FCvA',
+    """
+
+    from urlparse import urlparse, parse_qs
+
+    if url.startswith(('youtu', 'www')):
+        url = 'http://' + url
+        
+    query = urlparse(url)
+    
+    if 'youtube' in query.hostname:
+        if query.path == '/watch':
+            return parse_qs(query.query)['v'][0]
+        elif query.path.startswith(('/embed/', '/v/')):
+            return query.path.split('/')[2]
+    elif 'youtu.be' in query.hostname:
+        return query.path[1:]
+    else:
+        raise ValueError
